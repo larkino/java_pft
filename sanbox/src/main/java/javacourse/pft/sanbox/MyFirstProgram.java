0b5dacc68a76154b69bac6226944aca38dc222c5
larkino <larkin@live.com>
2017-01-22 20:59:45 +0200

    Добавлено пару функций в MyFirstProgram

--- sanbox/src/main/java/javacourse/pft/sanbox/MyFirstProgram.java
+++ sanbox/src/main/java/javacourse/pft/sanbox/MyFirstProgram.java
@@ -3,7 +3,33 @@ package javacourse.pft.sanbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello World");
+  //Вызов функции hello ();
+    hello("world");
+    hello("user");
+    hello("Oleksandr");
+    double l = 5;
+    System.out.println("Площадь квадарата со стороной " + l + " равна " + area(l));
+
+    double a = 4;
+    double b = 5;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b +  " равна " + area(a, b));
+
   }
+  // Функция Hello World;
+  public static void hello(String somebody ) {
+
+    System.out.println("Hello," + somebody + "!");
+  }
+
+  public  static double area (double len) {
+    return  len*len;
+
+  }
+
+  public static double area (double a, double b) {
+    return  a*b;
+  }
+
+
 
 }
